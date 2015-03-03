beca944e378c51fc3d3f99a77ed71b12f74b889e
Stephen Lavelle <stephenlavelle@live.com>
2015-03-03 15:34:13 +0000

    Updated Corner Turning
    Still a bit jumpy, will have to work on it more.

--- StephenProject/GamesFleadhTest/Assets/Scripts/GroundGeneration.cs
+++ StephenProject/GamesFleadhTest/Assets/Scripts/GroundGeneration.cs
@@ -4,53 +4,98 @@ using System.Collections.Generic;
 
 public class GroundGeneration : MonoBehaviour
 {
-    public GameObject _player, _ground;
+    public GameObject _player, _ground, _curvedGroundLeft, _curvedGroundRight, _curved;
+    GameObject _spawnPoint;
     public List<GameObject> _groundPieces;
-    bool _readyToSpawn = false;
+    bool _readyToSpawn = true;
     float _timeSinceLastSpawn = 0.0f;
+    public int _nextGroundPosition = 1;
+    int _numberSpawned;
 
     void Start()
     {
+        _nextGroundPosition = 1;
+        CreateGroundStraight();
+        _numberSpawned = 0;
 
     }
 
     void Update()
     {
-        CreateGround();
-        MoveGround();
-
-        if (_readyToSpawn == false)
+        _spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+        if(Input.GetKeyDown(KeyCode.Q))
         {
-            _timeSinceLastSpawn += Time.deltaTime;
-        }
-        if (_timeSinceLastSpawn >= Random.Range(.5f, 1.5f))
-        {
-            _readyToSpawn = true;
-            _timeSinceLastSpawn = 0;
+            CreateGroundStraight();
         }
     }
 
-    void CreateGround()
+    public void CreateGroundStraight()
     {
-        if (_readyToSpawn)
+        _spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+        _readyToSpawn = true;
+        if (_nextGroundPosition == 1)
         {
-            if (_groundPieces.Count < 11)
+            if (_readyToSpawn)
             {
-                for (int x = 0; x < 5; x++)
+                for (int i = 0; i < (int)Random.Range(10, 20); i++)
                 {
-                    _groundPieces.Add(_ground);
-                    Instantiate(_ground, new Vector3(0, 0, (_player.transform.position.z + (x * 8))), Quaternion.identity);
-                    _readyToSpawn = false;
+                    _spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+                    Instantiate(_ground, _spawnPoint.transform.position +  (_spawnPoint.transform.forward * (8 * i)), Quaternion.identity);
+                    _numberSpawned++;
                 }
+                Instantiate(_curvedGroundLeft, _spawnPoint.transform.position + (_spawnPoint.transform.forward * (_numberSpawned*8)), Quaternion.identity);
+                _nextGroundPosition = 0;
+                _readyToSpawn = false;
+                _numberSpawned = 0;
             }
         }
-    }
+        else if(_nextGroundPosition == 0)
+        {
+            print("RTS : "  +_readyToSpawn.ToString());
+            if (_readyToSpawn)
+            {
+                for (int i = 0; i < (int)Random.Range(10,20); i++)
+                {
+                    _spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+                    Instantiate(_ground, _spawnPoint.transform.position + (_spawnPoint.transform.forward * (8 * i)), Quaternion.identity);
+                    _numberSpawned++;
+                }
+                Instantiate(_curvedGroundRight, _spawnPoint.transform.position + (_spawnPoint.transform.forward * (_numberSpawned*8)), Quaternion.Euler(new Vector3(0,-90,0)));
+                _nextGroundPosition = 1;
+                _readyToSpawn = false;
+                _numberSpawned = 0;
+            }
+        }
 
-    void MoveGround()
-    {
-        
-    }
+        //#region SpawnLeft
+        //if (_nextGroundPosition == 1)
+        //{
+            
+        //        Instantiate(_ground, spawnPoint, Quaternion.identity);
+        //        _nextGroundPosition = 0;
+            
+        //        print("NTS : " + DestroyGround.numbertimesspawned);
+        //        _groundPieces.AddRange(GameObject.FindGameObjectsWithTag("Ground"));
+        //        Instantiate(_curvedGroundLeft, spawnPoint, Quaternion.identity);
 
+        //        DestroyGround.numbertimesspawned = 0;
+            
+        //}
+        //#endregion
+        //#region SpawnRight
+        //else if (_nextGroundPosition == 0)
+        //{
+        //        Instantiate(_ground, spawnPoint, Quaternion.identity);
+        //        _nextGroundPosition = 1;
+            
+        //        _groundPieces.AddRange(GameObject.FindGameObjectsWithTag("Ground"));
+        //        Instantiate(_curvedGroundLeft, spawnPoint, Quaternion.identity);
+                
+        //        DestroyGround.numbertimesspawned = 0;
+            
+        //}
+        //#endregion
+    }
 }
 
     
--- StephenProject/GamesFleadhTest/Assets/Scripts/PlayerCameraMovement.cs
+++ StephenProject/GamesFleadhTest/Assets/Scripts/PlayerCameraMovement.cs
@@ -3,56 +3,211 @@ using System.Collections;
 
 public class PlayerCameraMovement : MonoBehaviour 
 {
-    public GameObject _Camera;
+    public GameObject _Camera, _directionSetter, _controller;
     public static int _playerScore = 0;
     public GUIText _scoreText;
+    public static int _playerFacing;
+    int centrePos;
+    public float turnTime;
+    DestroyGround dest;
+
+
 	void Start ()
     {
-	
+        _playerFacing = 1;
+        centrePos = 1;
 	}
 	
 	void Update () 
     {
+        Debug.Log("Rotation " + _Camera.transform.rotation);
         Movement();
-        _Camera.transform.position = new Vector3(0, 4, transform.position.z - 8);
-        //transform.Rotate(3,0,0);
-        //_playerScore = 1 + (int)Time.time;
         _playerScore = (int)transform.position.z;
-        _scoreText.text = "Score : " + _playerScore;
+        _scoreText.text = "FPS : " + 1.0f / Time.deltaTime;
+        _directionSetter.transform.position = transform.position;
+        CameraSettings();
+        _controller = GameObject.FindGameObjectWithTag("Controller");
 	}
+
+    IEnumerator RotateCamera()
+    {
+        if (_playerFacing == 0)
+        {
+            while (_Camera.transform.rotation != Quaternion.Euler(0, -90, 0))
+            {
+                _Camera.transform.rotation = Quaternion.Slerp(_Camera.transform.rotation, Quaternion.Euler(0, -90, 0), .25f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        }
+        else if (_playerFacing == 1)
+        {
+            while (_Camera.transform.rotation != Quaternion.Euler(0, 0, 0))
+            {
+                _Camera.transform.rotation = Quaternion.Slerp(_Camera.transform.rotation, Quaternion.Euler(0, 00, 0), .25f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        }
+        CameraSettings();
+    }
+    void CameraSettings()
+    {
+
+        if (_playerFacing == 0)
+        {
+            //_Camera.transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
+            //_Camera.transform.rotation = Quaternion.Slerp(_Camera.transform.rotation, Quaternion.Euler(0, -90, 0), 1000);
+            _Camera.transform.position = new Vector3(transform.position.x + 8, 4, transform.position.z);
+        }
+        else if (_playerFacing == 1)
+        {
+            //_Camera.transform.rotation = Quaternion.Slerp(_Camera.transform.rotation, Quaternion.Euler(0, 0, 0), 1000);
+            //_Camera.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            _Camera.transform.position = new Vector3(transform.position.x, 4, transform.position.z - 8);
+        }
+    }
 
     void Movement()
     {
-        transform.rigidbody.AddForce(new Vector3(0, 0, 7));
-        //transform.position += new Vector3(0, 0, .5f);
-        if (transform.position.x > 0)
+        transform.rigidbody.AddForce(_directionSetter.transform.forward * 8, ForceMode.Acceleration);
+        ///Jump
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    transform.position += new Vector3(0, 2, 0);
+        //}
+        if (_playerFacing == 1)
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (centrePos == 2)
             {
-                transform.position = new Vector3(0, .755f, transform.position.z);
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position = new Vector3(transform.position.x-2, transform.position.y, transform.position.z);
+                    centrePos = 1;
+                }
+            }
+            else if (centrePos == 0)
+            {
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+                    centrePos = 1;
+                }
+            }
+            else if (centrePos == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    transform.position = new Vector3(transform.position.x - 2, transform.position.y, transform.position.z);
+                    centrePos = 0;
+                }
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    transform.position = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
+                    centrePos = 2;
+                }
             }
         }
-        else if (transform.position.x < 0)
+
+        else if (_playerFacing == 0)
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (centrePos == 2)
             {
-                transform.position = new Vector3(0, .755f, transform.position.z);
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+
+                    //Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z - 2), 50);
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2);
+                    centrePos = 1;
+                }
+            }
+            else if (centrePos == 0)
+            {
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    //Vector3.Lerp(transform.position, new Vector3(transform.position.x, .755f, transform.position.z + 2), 50);
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
+                    centrePos = 1;
+                }
+            }
+            else if (centrePos == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    //Vector3.Lerp(transform.position, new Vector3(transform.position.x, .755f, transform.position.z - 2), 50);
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 2);
+                    centrePos = 0;
+                }
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    //Vector3.Lerp(transform.position, new Vector3(transform.position.x, .755f, transform.position.z + 2), 50);
+                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
+                    centrePos = 2;
+                }
             }
         }
-        else if (transform.position.x == 0)
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "GCC")
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (_playerFacing == 1)
             {
-                transform.position = new Vector3(-2, .755f, transform.position.z);
+                _playerFacing = 0;
+                ///Camera Rotation
+                CameraSettings();
+                StartCoroutine("RotateCamera");
+                Quaternion target = Quaternion.Euler(0, -90, 0);
+                transform.localRotation = target;
+                centrePos = 1;
+                transform.rigidbody.freezeRotation = true;
+                rigidbody.velocity = new Vector3(-rigidbody.velocity.z, 0, 0);
+                _directionSetter.transform.localRotation = target;
+                _controller.GetComponent<GroundGeneration>().CreateGroundStraight();
+
+                Destroy(col.gameObject);
+                Debug.Log("CORNER" + _playerFacing);
             }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            else if (_playerFacing == 0)
             {
-                transform.position = new Vector3(2, .755f, transform.position.z);
+
+                _playerFacing = 1;
+                ///Camera Rotation
+                CameraSettings();
+                StartCoroutine("RotateCamera");
+                Quaternion target = Quaternion.Euler(0, 0, 0);
+                transform.rigidbody.freezeRotation = true;
+                transform.localRotation = target;
+                centrePos = 1;
+                rigidbody.velocity = new Vector3(0, 0, -rigidbody.velocity.x);
+                _directionSetter.transform.localRotation = target;
+                _controller.GetComponent<GroundGeneration>().CreateGroundStraight();
+                Destroy(col.gameObject);
+                Debug.Log("CORNER" + _playerFacing);
             }
         }
 
+        if (col.gameObject.tag == "Curved")
+        {
+            print("HSDFHSDFJSDFJKSDFKSD");
+        }
+    }
 
+    void OnTriggerExit(Collider col)
+    {
 
+    }
 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            this.rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+
+    }
+
+    void OnCollisionExit()
+    {
+        this.rigidbody.constraints = RigidbodyConstraints.None;
     }
 }
