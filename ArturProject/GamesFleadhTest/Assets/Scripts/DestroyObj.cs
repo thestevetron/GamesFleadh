dfe3135e62d8853f49c0a5ac895d548981bfbfd2
ArturPinheiro <s00150676@mail.itsligo.ie>
2015-03-09 20:20:35 +0000

    fixed some bugs

--- ArturProject/GamesFleadhTest/Assets/Scripts/DestroyObj.cs
+++ ArturProject/GamesFleadhTest/Assets/Scripts/DestroyObj.cs
@@ -14,22 +14,19 @@ public class DestroyObj : MonoBehaviour
         _controller = GameObject.FindGameObjectWithTag("Controller");
         to = GameObject.FindGameObjectWithTag("To");
         this.transform.Rotate(new Vector3(135, 0, 0));
-        to.transform.position = new Vector3(transform.position.x, transform.position.y + .6f, transform.position.z - 1);
+        if (to != null) to.transform.position = new Vector3(transform.position.x, transform.position.y + .6f, transform.position.z - 1);
         
 	}
 	
 
 	void Update ()
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, to.transform.rotation, Time.time * speed);
+        if (to != null) transform.rotation = Quaternion.Lerp(transform.rotation, to.transform.rotation, Time.time * speed);
         if(transform.position.z < (_player.transform.position.z - 5))
         {
             _controller.GetComponent<CreateObstacle>().Trees.Remove(this.gameObject);
             _controller.GetComponent<CreateObstacle>().Trees.Clear();
             Destroy(this.gameObject);
-
         }
-
-
 	}
 }
--- ArturProject/GamesFleadhTest/Assets/Scripts/ObjectsManager.cs
+++ ArturProject/GamesFleadhTest/Assets/Scripts/ObjectsManager.cs
@@ -6,9 +6,10 @@ public class ObjectsManager : MonoBehaviour {
 
     // Use this for initialization
 
-    public GameObject Object_, player;
+    public GameObject tree, grass, bush, mushrrom, player;
 
     List<Object> objects = new List<Object>();
+    List<Object> objectsPrefabs;
     Vector3 _spawnPoint;
     Vector3 direction;
     Vector3 lanepos, posoffset;
@@ -16,16 +17,16 @@ public class ObjectsManager : MonoBehaviour {
     private float timer = 0;
     public float SpawmTime = 0.05f;
     public int objsCount = 3, distMult = 16;
-    int objCounter = 0;
+    int objIndex = 0, xyoffset = 3;
     int otherObjCounter = 0;
-    int lane = 1, textHeight;
+    int lane = 1;
     public float posOffset = 2; 
 
     void Start()
     {
         direction = new Vector3(0, 0, 1);
         _spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
-        //textHeight = gameObject.renderer.material.GetTexture(0).height;
+        objectsPrefabs = new List<Object>() { tree, grass, bush, mushrrom };
     }
 
     void Update()
@@ -73,36 +74,47 @@ public class ObjectsManager : MonoBehaviour {
 
         while (!isAtTheEnd())
         {
+            switch (objIndex)
+            {
+                case 0://tree
+                    posoffset = new Vector3(0, 1.5f, 0);
+                    xyoffset = 6;
+                    break;
+                case 1://grass
+                    posoffset = new Vector3(0, 0.5f, 0);
+                    xyoffset = 6;
+                    break;
+                case 2://bush
+                    posoffset = new Vector3(0, 0.5f, 0);
+                    xyoffset = 5;
+                    break;
+                case 3://mushrrom 
+                    posoffset = new Vector3(0, 0.5f, 0);
+                    xyoffset = 5;
+                    break;
+                default:
+                    break;
+            }
+            
             switch (lane)
             {
                 case 0:
-                    objects.Add(Instantiate(Object_, _spawnPoint - (lanepos * 4) + (direction * (otherObjCounter * distMult)) + posoffset, Quaternion.identity));
+                    objects.Add(Instantiate(objectsPrefabs[objIndex], _spawnPoint - (lanepos * xyoffset) + posoffset + (direction * (otherObjCounter * distMult)), Quaternion.identity));
                     break;
                 //case 1:
                 //    objects.Add(Instantiate(Object_, _spawnPoint + (direction * (otherObjCounter * distMult)), Quaternion.identity));
                 //    break;
                 case 1:
-                    objects.Add(Instantiate(Object_, _spawnPoint + (lanepos * 4) + (direction * (otherObjCounter * distMult) + posoffset), Quaternion.identity));
+                    objects.Add(Instantiate(objectsPrefabs[objIndex], _spawnPoint + (lanepos * xyoffset) + posoffset + (direction * (otherObjCounter * distMult)), Quaternion.identity));
                     break;
                 default:
                     break;
             }
 
-            //for (int i = 0; i < GetComponent<CollectManager>().collects.Count; i++)
-            //{
-            //    Object col = GetComponent<CollectManager>().collects[i];
-
-            //    if ((col as GameObject).transform.position == (objects[otherObjCounter] as GameObject).transform.position)
-            //    {
-            //        Debug.Log("msm lugar");
-            //        GetComponent<CollectManager>().removeCollect(col);
-            //    }
-            //}
-
-            //timer = 0;
             otherObjCounter++;
 
             lane = Random.Range(0, 2);
+            objIndex = Random.Range(0,4);
             //Debug.Log("instanciando");
         }
     }
@@ -118,12 +130,10 @@ public class ObjectsManager : MonoBehaviour {
         if (face == 0)
         {
             direction = new Vector3(-1, 0, 0);
-            posoffset = new Vector3(-posOffset, 1.5f , 0);
         }
         else
         {
             direction = new Vector3(0, 0, 1);
-            posoffset = new Vector3(0, 1.5f, posOffset);
         }
         _spawnPoint = player.transform.position;
 
